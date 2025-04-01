56faccf5df3fb103093040e6267ff53b102cb217
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-04-01 12:07:51 +0200

    changes to main.js

--- week-09/feedback-website/js/main.js
+++ week-09/feedback-website/js/main.js
@@ -21,15 +21,75 @@ document.addEventListener("DOMContentLoaded", () => {
   const nameError = document.getElementById("name-error")
   const emailError = document.getElementById("email-error")
 
-  // Remove errors
-  const clearErrors = (input, errorElement) => {
-    input.removeAttribute("aria-invalid", "true")
+  // Remove old errors
+  const clearError = (input, errorElement) => {
+    input.removeAttribute("aria-invalid")
     errorElement.textContent = ""
     errorElement.hidden = true
   }
 
+  const showError = (input, errorElement, message) => {
+    input.setAttribute("aria-invalid", "true")
+    errorElement.textContent = message
+    errorElement.hidden = false
+  }
+
+  function isValidEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+  }
+
   //Get the name and email inputs and the error messages
   nameInput.addEventListener("input", () => {
-    if (nameInput.value.trim()) clearErrors(nameInput, nameError)
+    if (nameInput.value.trim()) {
+      clearError(nameInput, nameError)
+    }
+  })
+
+  emailInput.addEventListener("input", () => {
+    if (emailInput.value.trim()) {
+      if (isValidEmail(emailInput.value)) {
+        clearError(emailInput, emailError)
+      }
+    }
+  })
+
+  userInfoForm.addEventListener("submit", (event) => {
+    // Prevent the form from reloading when submitting the form
+    event.preventDefault()
+    // Check if the name input is empty
+    let isValid = true
+
+    if (!nameInput.value.trim()) {
+      showError(nameInput, nameError, "Please enter your name.")
+      isValid = false
+      // Move focus to the name input where the error is
+      nameInput.focus()
+    } else {
+      clearError(nameInput, nameError)
+    }
+
+    // Check if the email input is empty or invalid
+    if (!emailInput.value.trim()) {
+      showError(emailInput, emailError, "Please enter your email.")
+      isValid = false
+      if (!nameError.textContent) {
+        // Move focus to the email input where the error is (if name error is not present)
+        emailInput.focus()
+      }
+    } else if (!isValidEmail(emailInput.value.trim())) {
+      showError(emailInput, emailError, "Please enter a valid email address.")
+      isValid = false
+    } else {
+      clearError(emailInput, emailError)
+    }
+
+    if (isValid) {
+      // Continue here if the form is valid
+      userName = nameInput.value.trim()
+      userInfoSection.hidden = true
+
+      announcer.textContent = "Moved to feedback section."
+      // Show the feedback section
+    }
   })
 })
